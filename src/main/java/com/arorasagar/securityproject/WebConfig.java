b7ba80ff64d2146e9d9aa8a9b3bb6bb0521bb9cc
Sagar <arorasagar1811@gmail.com>
2022-06-18 15:16:36 -0700

    added inmemmory authentication

--- src/main/java/com/arorasagar/securityproject/WebConfig.java
+++ src/main/java/com/arorasagar/securityproject/WebConfig.java
@@ -1,15 +1,56 @@
 package com.arorasagar.securityproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
+@Configuration
 public class WebConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired PasswordEncoder passwordEncoder;
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password(passwordEncoder.encode("User1Pass"))
+                .authorities("ROLE_USER");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http.authorizeRequests()
+                .antMatchers("/securityNone").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        http.cors()
+                .configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+    }
+
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("sagar")
+                .password(passwordEncoder.encode("abc123"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
+
+
